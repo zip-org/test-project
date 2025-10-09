1c6626c68306481e4e626a2a441f903210f3c9d3
Zipeng WU <zipeng.wu@sonarsource.com>
2025-10-09 16:55:39 +0200

    more issue

--- hotspot.py
+++ hotspot.py
@@ -1,22 +1,22 @@
-from aws_cdk import (
-    aws_sagemaker as sagemaker
-)
+from aws_cdk import aws_sagemaker as sagemaker
+
 
 class CfnSagemakerStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
         sagemaker.CfnNotebookInstance(
-            self, "Sensitive",
-            instance_type="instanceType",
-            role_arn="roleArn"
+            self, "Sensitive", instance_type="instanceType", role_arn="roleArn"
         )  # Sensitive, no KMS key is set by default; thus, encryption is disabled
 
 
 class MyClass:
     def __bool__(self):
         i = 1
-        return 0 # Noncompliant: Return value of type bool here.
+        j = 2
+        j = 3
+        return 0  # Noncompliant: Return value of type bool here.
+
 
 obj1 = MyClass()
-print(bool(obj1)) # TypeError: __bool__ should return bool, returned int
+print(bool(obj1))  # TypeError: __bool__ should return bool, returned int
