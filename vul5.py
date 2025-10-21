b68f74557dd7c9acb82ef3f201b9a9a3e62abfc2
Zipeng WU <zipeng.wu@sonarsource.com>
2025-10-21 09:21:25 +0200

    moar issue

--- vul5.py
+++ vul5.py
@@ -1,3 +1,8 @@
 import jwt
 
+jwt.decode(token, options={"verify_signature": False})
+jwt.decode(token, options={"verify_signature": False})
+jwt.decode(token, options={"verify_signature": False})
+jwt.decode(token, options={"verify_signature": False})
+jwt.decode(token, options={"verify_signature": False})
 jwt.decode(token, options={"verify_signature": False})  # Noncompliant
