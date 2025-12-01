82ec1256e54f542353bdeae8e55514395463851f
Zipeng WU <zipeng.wu@sonarsource.com>
2025-12-01 15:01:32 +0100

    moar issue

--- vul3.py
+++ vul3.py
@@ -1,8 +1,11 @@
 from flask import Flask, request, send_from_directory
 
-app = Flask('example')
+app = Flask("example")
 
-@app.route('/example')
+
+@app.route("/example")
 def example():
-    my_file = request.args['my_file']
-    return send_file("static/%s" % my_file, as_attachment=True) # Noncompliant
+    my_file = request.args["my_file"]
+    i = 3
+    i = 4
+    return send_file("static/%s" % my_file, as_attachment=True)  # Noncompliant
