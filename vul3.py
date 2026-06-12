9a0f0ae2c1ed2b028c0cbd147cd9ff1c9438e9dc
Zipeng WU <zipeng.wu@sonarsource.com>
2026-06-12 16:54:15 +0200

    add issue

--- vul3.py
+++ vul3.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, send_from_directory
+from mysql.connector import connection
 
 app = Flask("example")
 
@@ -8,4 +9,15 @@ def example():
     my_file = request.args["my_file"]
     i = 3
     i = 4
+    i = 5
+    i = 6
     return send_file("static/%s" % my_file, as_attachment=True)  # Noncompliant
+
+
+def add_to_list(item, my_list=[]):
+    my_list.append(item)
+    return my_list
+
+
+print(add_to_list(1))  # Output: [1]
+print(add_to_list(2))
