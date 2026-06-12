583ac0752f814872288250d7cba636796ec6e3f9
Zipeng WU <zipeng.wu@sonarsource.com>
2026-06-12 17:54:27 +0200

    add issue

--- vul4.py
+++ vul4.py
@@ -1,4 +1,5 @@
 import jwt
+from mysql.connector import connection
 
 jwt.decode(token, options={"verify_signature": False})  # Noncompliant
 jwt.decode(token, options={"verify_signature": False})  # Noncompliant
